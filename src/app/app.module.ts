b77b8aaf8f2ea4ce7bf524e313fb16ac0fdbc2e6
Mohsin Firoz <m7firoz@gmail.com>
2020-12-03 09:29:05 +1100

    fixed layout and added routing button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbListModule, NbCardModule, NbButtonModule} from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbListModule, NbCardModule, NbButtonModule, NbInputModule, NbFormFieldModule, NbIconModule} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { ProductService } from './product.service';
@@ -29,6 +29,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NbListModule,
     NbCardModule,
     NbButtonModule,
+    NbInputModule,
+    NbFormFieldModule,
+    NbIconModule,
     MatGridListModule,
     FormsModule
   ],

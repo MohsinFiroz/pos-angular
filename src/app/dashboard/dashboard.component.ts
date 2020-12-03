ff338e76ae7007087e9bd24020057819d3948b06
Mohsin Firoz <m7firoz@gmail.com>
2020-12-03 11:01:12 +1100

    css tricks

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
 

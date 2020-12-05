2dbd0cd6cb2d02b33119ef8477a726f824682ad6
Mohsin Firoz <m7firoz@gmail.com>
2020-12-06 06:51:32 +1100

    Updated to Angular 11

--- src/app/product-update-dialog/product-update-dialog.component.spec.ts
+++ src/app/product-update-dialog/product-update-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductUpdateDialogComponent } from './product-update-dialog.component';
 
@@ -6,7 +6,7 @@ describe('ProductUpdateDialogComponent', () => {
   let component: ProductUpdateDialogComponent;
   let fixture: ComponentFixture<ProductUpdateDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductUpdateDialogComponent ]
     })
